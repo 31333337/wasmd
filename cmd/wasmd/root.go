5797d1176dc62265186e83a70706c757bf2bee7a
Hans Bricks <hans@dialout.net>
2023-08-10 13:29:37 +0000

    change names to boomerangwq

--- cmd/wasmd/root.go
+++ cmd/wasmd/root.go
@@ -65,7 +65,7 @@ func NewRootCmd() (*cobra.Command, params.EncodingConfig) {
 
 	rootCmd := &cobra.Command{
 		Use:   version.AppName,
-		Short: "Wasm Daemon (server)",
+		Short: "0KNetwork's b00merang Daemon (server)",
 		PersistentPreRunE: func(cmd *cobra.Command, _ []string) error {
 			// set the default command outputs
 			cmd.SetOut(cmd.OutOrStdout())
